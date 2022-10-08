7775a9ea98b9355292b3423acff4cac30d674006
Sushant <sushantgangwar87@gmail.com>
2022-10-08 09:45:46 +0530

    Fixed Router Issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useEffect } from "react";
 // import { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer.jsx";
@@ -20,23 +18,18 @@ const App = () => {
     }, [location]);
 
     return (
-        <BrowserRouter>
-            <div className="app">
-                <Navbar />
-                {/* <Suspense> */}
-                <Routes location={location} key={location.pathname}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/" element={<OurStory />} />
-                    <Route path="/locations" element={<Locations />} />
-                    <Route
-                        path="/location/details/:location"
-                        element={<LocationDetails />}
-                    />
-                </Routes>
-                {/* </Suspense> */}
-                <Footer />
-            </div>
-        </BrowserRouter>
+        <div className="app">
+            <Navbar />
+            {/* <Suspense> */}
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Home />} />
+                <Route path="/" element={<OurStory />} />
+                <Route path="/locations" element={<Locations />} />
+                <Route path="/location/details/:location" element={<LocationDetails />} />
+            </Routes>
+            {/* </Suspense> */}
+            <Footer />
+        </div>
     );
 };
 
